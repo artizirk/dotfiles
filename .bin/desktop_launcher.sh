402ede807f12eef9728f9b7ee28c50bfa777f297
Arti Zirk <arti.zirk@gmail.com>
2018-11-19 17:10:32 +0200

    Implement fixes by mikroskeem

--- .bin/desktop_launcher.sh
+++ .bin/desktop_launcher.sh
@@ -3,10 +3,12 @@ oIFS=$IFS
 IFS=:
 data_dirs=($XDG_DATA_DIRS)
 IFS=$oIFS
-data_dirs+=("~/.local/share")
-(
-    for folder in ${data_dirs[*]}; do 
-        find ${folder%/}/applications -name \*.desktop;
-    done
-) | xargs basename -s .desktop -a | fzf | xargs -r swaymsg -t command exec gtk-launch
+data_dirs+=("${HOME}/.local/share")
 
+fzf_history="${HOME}/.desktop_launcher_history"
+
+for folder in ${data_dirs[*]}; do find "${folder%/}/applications" -name '*.desktop' -print0; done \
+    | xargs -0 basename -z -s .desktop -a \
+    | sort -r -z \
+    | fzf +m --read0 --history="${fzf_history}" \
+    | xargs -r swaymsg -t command exec gtk-launch
