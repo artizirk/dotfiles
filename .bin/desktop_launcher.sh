405bb65393761cf0e79e01e8bb45b70234b74adb
Arti Zirk <arti.zirk@gmail.com>
2019-07-05 12:45:20 +0300

    Switch launcher to i3-dmenu-desktop

--- .bin/desktop_launcher.sh
+++ .bin/desktop_launcher.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-oIFS=$IFS
-IFS=:
-data_dirs=($XDG_DATA_DIRS)
-IFS=$oIFS
-data_dirs+=("${HOME}/.local/share")
-
 fzf_history="${HOME}/.desktop_launcher_history"
 
-for folder in ${data_dirs[*]}; do find "${folder%/}/applications" -name '*.desktop' -print0; done \
-    | xargs -0 basename -z -s .desktop -a \
-    | sort -r -z \
-    | fzf +m --read0 --history="${fzf_history}" \
-    | xargs -r swaymsg -t command exec gtk-launch
+i3-dmenu-desktop --dmenu="fzf +m --history=\"${fzf_history}\""
