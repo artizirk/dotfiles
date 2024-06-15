f42b5eb4aceb181c83dbb629c2f61ab02f9ebd92
Arti Zirk <arti.zirk@gmail.com>
2024-06-15 11:26:24 +0300

    Switch from tvolnoti to swayosd

--- .bin/volume_subscribe.sh
+++ .bin/volume_subscribe.sh
@@ -4,7 +4,7 @@ pactl subscribe | grep --line-buffered "sink" |
 while read; do
 	VOL=$(amixer get Master | grep -Po "[0-9]+(?=%)" | tail -1)
 	if [[ $VOL != $OLD_VOL ]]; then
-		tvolnoti-show $VOL
+		swayosd-client --output-volume=+0
 		OLD_VOL=$VOL
 	fi
 done
